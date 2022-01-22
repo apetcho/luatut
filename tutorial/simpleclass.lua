21f6d5413d3362ffa0b3860371559a83d84950a8
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 15:13:53 +0100

    Introduce class emulation in Lua

---
+++ tutorial/simpleclass.lua
@@ -0,0 +1,19 @@
+------------------------------------
+--- Simple Metatable-based class ---
+------------------------------------
+
+--[[
+    The table representing the class, which will double as the metatable for
+    the instances
+--]]
+local MyClass = {}
+-- Failed table lookups on the instances should fallback to the class table,
+-- to get methods
+MyClass.__index = MyClass
+
+--[[ Syntax equivalent to 'MyClass.new = function ...']]
+function MyClass.new(init)
+    local self = setmetatable({}, MyClass)
+    self.value = init
+    return self
+end
