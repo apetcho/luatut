9c2d5e9ee580c64f941739fb4e782159e12557fe
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 14:16:49 +0100

    Learn the contrast between global and local variables

--- tutorial/sqr.lua
+++ tutorial/sqr.lua
@@ -6,4 +6,19 @@ end
 
 local y = sqr(10)
 print("y is", y)
-print("x is", x)
+-- print("x is", x)
+
+---------------------------------
+-- local and global variables ---
+---------------------------------
+function sum_numbers(n)
+    local sum = 0
+    for i = 1, n do
+        sum = sum + i
+    end
+    return sum
+end
+
+local result = sum_numbers(10)
+io.write("result = ", result)
+print()
